8c21de9f5fd6df26554492bb6700d8758f7b6146
SayakDas1 <64456749+SayakDas1@users.noreply.github.com>
2023-03-05 23:15:48 +0530

    Add files via upload

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Product, Contact, Order, OrderUpdate
+from .models import Product, Contact, Orders, OrderUpdate
 
 # Register your models here(To show model in admin!)
 admin.site.register(Product)
 admin.site.register(Contact)
-admin.site.register(Order)
+admin.site.register(Orders)
 admin.site.register(OrderUpdate)
